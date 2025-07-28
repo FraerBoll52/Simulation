c20a3deaffbe2a1be89f722feb7a72ebad60086b
FraerBoll <kzeltov110@gmail.com>
2025-07-28 15:45:10 +0400

    Cleared lines and methods and putted them into right place - Coordinates class

--- src/Entitys/Entity.java
+++ src/Entitys/Entity.java
@@ -1,12 +1,7 @@
 package Entitys;
 
 public abstract class Entity {
-    private int x;
-    private int y;
 
-    public boolean isAt(int x, int y) {
-        return this.x == x && this.y == y;
-    }
 
 
 }
