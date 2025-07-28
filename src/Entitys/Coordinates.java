2fbc65c431ec0de0f5e4fdd892ea84e70670e812
FraerBoll <kzeltov110@gmail.com>
2025-07-28 15:43:50 +0400

    Added overrides of hashCode and equals to avoid collision (one entity-one cell), also added getters and isAt() (may be deleted next time because already have getters)

--- src/Entitys/Coordinates.java
+++ src/Entitys/Coordinates.java
@@ -1,4 +1,46 @@
 package Entitys;
 
+import java.util.Objects;
+
 public class Coordinates {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+
+
+    public int getY() {
+        return y;
+    }
+
+
+
+    public boolean isAt(int x, int y) {
+        return this.x == x && this.y == y;
+    }
+    //для отладки
+
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
